9dd8dcff59d0e605cbd8889afaff1b0c75660657
karin27-06 <karinsntmr@gmail.com>
2025-10-30 18:19:21 -0500

    Table Factory

--- database/factories/TableFactory.php
+++ database/factories/TableFactory.php
@@ -14,7 +14,7 @@ class TableFactory extends Factory
     {
         return [
             'name'      => 'Mesa ' . self::$counter++, // Genera Mesa 1, Mesa 2, Mesa 3, etc.
-            'tablenum'  => $this->faker->unique()->numerify('TBL-###'),
+            'tablenum'  => $this->faker->numerify('TBL-###'),
             'idArea'    => Areas::inRandomOrder()->first()->id,
             'idFloor'   => Floor::inRandomOrder()->first()->id,
             'capacity'  => $this->faker->numberBetween(2, 10),
