e64e9f623250a6fcbded0bf89d6207d528da3418
karin27-06 <karinsntmr@gmail.com>
2025-11-20 16:50:21 -0500

    TS-Arreglado-2

--- database/factories/TableFactory.php
+++ database/factories/TableFactory.php
@@ -2,19 +2,27 @@
 
 namespace Database\Factories;
 
+use App\Models\Table;
 use App\Models\Areas;
 use App\Models\Floor;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class TableFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Table::class;
+
     protected static int $counter = 1; // Contador estático para numerar las mesas
 
     public function definition(): array
     {
         return [
-            'name'      => 'Mesa ' . self::$counter++, // Genera Mesa 1, Mesa 2, Mesa 3, etc.
-            'tablenum' => 'TBL-' . rand(1, 999),
+            'name'      => 'Mesa ' . self::$counter++, // Genera Mesa 1, Mesa 2, etc.
+            'tablenum'  => 'TBL-' . $this->faker->unique()->numberBetween(1, 999),
             'idArea'    => Areas::inRandomOrder()->first()->id,
             'idFloor'   => Floor::inRandomOrder()->first()->id,
             'capacity'  => $this->faker->numberBetween(2, 10),
